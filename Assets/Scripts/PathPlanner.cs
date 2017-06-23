66b98f97fcb22142c67e7f9cb9da5b8f8ada6202
Dwatkins <dexter.a.watkins@vanderbilt.edu>
2017-06-23 13:57:53 -0500

    added some microsoft tutorials

---
+++ Assets/Scripts/PathPlanner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathPlanner : MonoBehaviour {
+
+    public UDPCommunication comm;
+
+    bool enabled;
+
+    // Use this for initialization
+    void Start () {
+
+        enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Enable()
+    {
+        enabled = true;
+    }
+
+    private void Disable()
+    {
+        enabled = false;
+    }
+}
